77465cf63be13d93fe85991bb156f1da65da188b
Naman  Singh <naman.singh.254@gmail.com>
2026-01-13 23:55:28 +0530

    Initial commit

---
+++ src/app/(home)/navbar.tsx
@@ -0,0 +1,19 @@
+import Link from "next/link";
+import Image from "next/image";
+import { SearchInput } from "./search-input";
+import { UserButton } from "@clerk/nextjs";
+
+export const Navbar = () => {
+    return (
+        <nav className="flex items-center justify-between h-full w-full">
+            <div className="flex items-center gap-3 shrink-0 pr-6">
+                <Link href="/">
+                <Image src="/logo.svg" alt="Logo" width={32} height={32}/>
+                </Link>
+                <h3 className="text-xl">Docs</h3>
+            </div>
+            <SearchInput/>
+            <UserButton/> 
+        </nav>
+    );
+};
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,12 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
+import { Inter } from "next/font/google";
 import "./globals.css";
+import {NuqsAdapter} from "nuqs/adapters/next/app";
+import { ConvexClientProvider } from "@/components/convex-client-provider";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+const inter =  Inter({
+  subsets: ["latin"],
+  
 });
 
 export const metadata: Metadata = {
@@ -25,10 +21,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
+      <body className={inter.className}>
+        <NuqsAdapter>
+          <ConvexClientProvider>
+            {children}
+          </ConvexClientProvider>
+        </NuqsAdapter>
       </body>
     </html>
   );
