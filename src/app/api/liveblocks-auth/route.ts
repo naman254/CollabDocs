875b34c8eb79c8cfc0b2fcaf08c8514206564de8
Naman  Singh <naman.singh.254@gmail.com>
2026-01-16 23:24:49 +0530

    fixed a typo

--- src/app/api/liveblocks-auth/route.ts
+++ src/app/api/liveblocks-auth/route.ts
@@ -27,7 +27,7 @@ export async function POST(req: Request) {
 
     const isOwner = document.ownerId === user.id;
     const isOrganizationMember = 
-    !!(document.organizationId && document.organizationId === sessionClaims.org_id);
+    !!(document.organizationId && document.organizationId === sessionClaims.organization_id);
 
     if(!isOwner && !isOrganizationMember){
         return new Response("Unauthorized", { status: 401 });
